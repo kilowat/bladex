6aec3636fff37111b4b2210bd13f0467b0d54f00
Sergey <kadantsev.sergey@gmail.com>
2025-05-29 12:24:25 +0300

    work

--- views/home/index.blade.php
+++ views/home/index.blade.php
@@ -1,21 +1,7 @@
-<?php
-// Подход 1: PHPDoc аннотации для шаблонов
-// /local/views/home/index.blade.php
-
-/**
- * Home page template
- * 
- * @param string $siteName - Название сайта
- * @param array $news - Массив новостей с полями: title, date, content
- * @param bool $showSidebar - Показывать ли боковую панель
- * @param string $metaTitle - Заголовок для meta
- * @param string $metaDescription - Описание для meta
- */
-?>
+@php /** @var \Views\Home\IndexViewModel $data */ @endphp
 
 @extends('layouts.app')
 
-@section('title', $title)
 
 @push('styles')
     <style>
@@ -26,6 +12,7 @@
 @endpush
 
 @section('content')
+    {{ $data->name }}
     <h2>{{ $title }}</h2>
     <p>{{ $description }}</p>
 
