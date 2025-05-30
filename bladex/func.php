fa279ac312823d0288375a9b6fa8925e14a45d75
Sergey <kadantsev.sergey@gmail.com>
2025-05-30 11:01:21 +0300

    work

--- bladex/func.php
+++ bladex/func.php
@@ -1,20 +1,12 @@
 <?php
 
-if (!function_exists('view')) {
-    /**
-     * Глобальная функция для рендеринга Blade шаблонов
-     */
-    function view(string $template, array $data = []): Bitrix\Main\HttpResponse
-    {
-        return Bladex\BladeRenderer::getInstance()->render($template, $data);
-    }
+function view(string $template, array $data = []): Bitrix\Main\HttpResponse
+{
+    return Bladex\BladeRenderer::getInstance()->render($template, $data);
 }
 
-
-if (!function_exists('route')) {
-    function route(string $name, array $params = [])
-    {
-        $router = \Bitrix\Main\Application::getInstance()->getRouter();
-        return $router->route($name, $params);
-    }
+function route(string $name, array $params = [])
+{
+    $router = \Bitrix\Main\Application::getInstance()->getRouter();
+    return $router->route($name, $params);
 }
--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,7 @@ use Bitrix\Main\Routing\RoutingConfigurator;
 
 return function (RoutingConfigurator $routes) {
     $routes->get('/', [App\Controllers\HomeController::class, 'index'])->name('home');
+    $routes->get('/test', function () {
+        return 'test';
+    });
 };
