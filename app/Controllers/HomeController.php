5f601fb3ce81914e4f7749e059aa132830bfd128
Sergey <kadantsev.sergey@gmail.com>
2025-06-01 13:25:40 +0300

    work

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -1,8 +1,11 @@
 <?php
 namespace App\Controllers;
+
+use App\Services\DataBaseService;
 class HomeController extends BaseController
 {
-    public function indexAction()
+
+    public function indexAction(DataBaseService $service)
     {
         return view('home.index', []);
     }
---
+++ app/Services/DataBaseService.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\Services;
+
+class DataBaseService
+{
+
+    public function getData()
+    {
+        return ['data'];
+    }
+}
